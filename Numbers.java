97f20d034fda521169a1ae28308341d82e8a9d97
Eric Maina <ericalmaina00@gmail.com>
2026-03-04 14:41:38 +0300

    Add Numbers.java to print numbers from 100 to 50

---
+++ Numbers.java
@@ -0,0 +1,11 @@
+//WEEK3 ACTIVITY 3
+//ERIC MAINA
+//ct100/g/21350/24
+public class Numbers {
+    public static void main(String[] args) {
+        // Start at 100, continue as long as i >= 50, decrease by 1 each time
+        for (int i = 100; i >= 50; i--) {
+            System.out.println(i);
+        }
+    }
+}
