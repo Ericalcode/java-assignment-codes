135cafe03708e1c23aed7833aacdb9692f8d5677
Eric Maina <ericalmaina00@gmail.com>
2026-03-04 13:48:10 +0300

    Remove commented lines for cleaner output

---
+++ Units.java
@@ -0,0 +1,16 @@
+//CT100/g/21350/24
+//Eric Maina
+//WEEK 1 task 1 asignment
+public class Units {
+    public static void main(String[] args) {
+        
+        System.out.println("Units Offered This Semester:");
+        
+        
+        System.out.println("1. Introduction to Programming");
+        System.out.println("2. Data Structures");
+        System.out.println("3. Computer Networks");
+        System.out.println("4. Database Systems");
+        System.out.println("5. Software Engineering");
+    }
+}
