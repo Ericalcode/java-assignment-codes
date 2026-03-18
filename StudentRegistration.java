24e25233a16f0c38d9a34715107962e5a8669d34
Eric Maina <ericalmaina00@gmail.com>
2026-03-18 09:30:35 +0300

    Calculator

--- StudentRegistration.java
+++ StudentRegistration.java
@@ -1,85 +1,74 @@
+//ct100/g/21350/24
+//Eric Maina
+//Calculator
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class StudentRegistration extends JFrame implements ActionListener {
+public class Calculator extends JFrame implements ActionListener {
 
-    JTextField id, name, course, year;
-    JComboBox dept;
-    JRadioButton m, f, o;
-    JCheckBox hostel;
-    JTextArea area;
-    JButton reg, clear;
+    JTextField t;
+    double num1;
+    String op;
 
-    public StudentRegistration() {
-        setTitle("Student Registration");
-        setSize(400,400);
-        setLayout(new GridLayout(0,2));
+    public Calculator(){
+        setTitle("Calculator");
+        setSize(300,300);
+        setLayout(new GridLayout(5,4));
 
-        id = new JTextField();
-        name = new JTextField();
-        course = new JTextField();
-        year = new JTextField();
+        t = new JTextField();
+        add(t);
 
-        dept = new JComboBox(new String[]{"IT","Business","Engineering","Science"});
+        String b[]={"7","8","9","/",
+                    "4","5","6","*",
+                    "1","2","3","-",
+                    "0",".","=","+",
+                    "C"};
 
-        m = new JRadioButton("Male");
-        f = new JRadioButton("Female");
-        o = new JRadioButton("Other");
-
-        ButtonGroup bg = new ButtonGroup();
-        bg.add(m); bg.add(f); bg.add(o);
-
-        hostel = new JCheckBox("Hostel");
-
-        reg = new JButton("Register");
-        clear = new JButton("Clear");
-
-        area = new JTextArea();
-
-        add(new JLabel("ID")); add(id);
-        add(new JLabel("Name")); add(name);
-        add(new JLabel("Course")); add(course);
-        add(new JLabel("Year")); add(year);
-        add(new JLabel("Dept")); add(dept);
-
-        add(m); add(f);
-        add(o); add(hostel);
-
-        add(reg); add(clear);
-        add(area);
-
-        reg.addActionListener(this);
-        clear.addActionListener(this);
+        for(int i=0;i<b.length;i++){
+            JButton btn=new JButton(b[i]);
+            add(btn);
+            btn.addActionListener(this);
+        }
 
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==reg){
-            if(id.getText().equals("") || name.getText().equals("")){
-                JOptionPane.showMessageDialog(this,"Fill all fields");
-                return;
-            }
+    public void actionPerformed(ActionEvent e){
+        String s=e.getActionCommand();
 
-            String gender = m.isSelected()?"Male":f.isSelected()?"Female":"Other";
-
-            area.setText("ID: "+id.getText()+
-            "\nName: "+name.getText()+
-            "\nDept: "+dept.getSelectedItem()+
-            "\nGender: "+gender+
-            "\nHostel: "+(hostel.isSelected()?"Yes":"No"));
+        if(s.matches("[0-9.]")){
+            t.setText(t.getText()+s);
         }
+        else if(s.matches("[+\\-*/]")){
+            num1=Double.parseDouble(t.getText());
+            op=s;
+            t.setText("");
+        }
+        else if(s.equals("=")){
+            double num2=Double.parseDouble(t.getText());
+            double r=0;
 
-        if(e.getSource()==clear){
-            id.setText(""); name.setText("");
-            course.setText(""); year.setText("");
-            area.setText("");
+            if(op.equals("+")) r=num1+num2;
+            if(op.equals("-")) r=num1-num2;
+            if(op.equals("*")) r=num1*num2;
+            if(op.equals("/")){
+                if(num2==0){
+                    JOptionPane.showMessageDialog(this,"Error");
+                    return;
+                }
+                r=num1/num2;
+            }
+            t.setText(""+r);
+        }
+        else if(s.equals("C")){
+            t.setText("");
         }
     }
 
-    public static void main(String[] args) {
-        new StudentRegistration();
+    public static void main(String[] args){
+        new Calculator();
     }
 }
